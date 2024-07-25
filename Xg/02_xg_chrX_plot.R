645000b06a9089d67a0f53e35613c834d1bfad88
xgao32 <6380557+xgao32@users.noreply.github.com>
2024-07-25 17:03:34 +0000

    figure

--- Xg/02_xg_chrX_plot.R
+++ Xg/02_xg_chrX_plot.R
@@ -1,5 +1,33 @@
 # Manhattan plot and QQ plot for GWAS result for chromosome X SNPs associated with Xg 
 # see https://sahirbhatnagar.com/manhattanly/articles/web_only/manhattanly_full.html
 
-#library("manhattanly")
-1+1
+library("manhattanly")
+library(plotly)
+library(htmlwidgets)
+
+
+
+
+set.seed(12345)
+HapMap.subset <- subset(HapMap, CHR %in% 4:7)
+# for highlighting SNPs of interest
+significantSNP <- sample(HapMap.subset$SNP, 20)
+head(HapMap.subset)
+
+dim(HapMap.subset)
+
+qqly(HapMap.subset, snp = "SNP", gene = "GENE")
+
+volcanoly(HapMap.subset, snp = "SNP", gene = "GENE", effect_size = "EFFECTSIZE")
+
+manplot <- manhattanly(HapMap.subset, snp = "SNP", gene = "GENE",
+            annotation1 = "DISTANCE", annotation2 = "EFFECTSIZE",
+            highlight = significantSNP)
+
+# Convert to ggplot
+static_plot <- ggplotly(manplot)
+
+# Save using ggsave
+ggsave("manhattan_plot.png", static_plot, width = 10, height = 6, dpi = 300)
+
+saveWidget(manplot, "manhattan_plot.html")
