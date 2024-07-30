b0a509fcbdef7ce44ea1a9f157b27a50db3c06af
Xiaofeng Li <lixiaofeng0603@gmail.com>
2024-07-30 13:30:03 -0400

    test script for phenotype generate

--- gwas/generate_phenotype.py
+++ gwas/generate_phenotype.py
@@ -1,13 +1,13 @@
 import hail as hl
 import pandas as pd
-
+import sys
 
 def create_phenotype_file(vcf_file, chrom, pos, ref, alt, output_file):
     hl.init()
 
     # read vcf
     mt = hl.import_vcf(vcf_file, reference_genome='GRCh37')
-    print("sucess import")
+    print("success import")
     # set variant
     variant = mt.filter_rows(
         (mt.locus.contig == chrom) &
@@ -15,7 +15,7 @@ def create_phenotype_file(vcf_file, chrom, pos, ref, alt, output_file):
         (mt.alleles[0] == ref) &
         (mt.alleles[1] == alt)
     )
-
+    print("success create variant profile")
     # find variant
     if variant.count_rows() == 0:
         print("Variant not found in the VCF file.")
@@ -23,10 +23,10 @@ def create_phenotype_file(vcf_file, chrom, pos, ref, alt, output_file):
 
     geno = variant.GT.collect()
     sample_ids = variant.s.collect()
-
+    print("success collect variants")
     # 0|0: 2 (variant), 0|1, 1|0, 1|1: 1 (control)
     phenotype = [2 if gt.is_hom_ref() else 1 for gt in geno]
-
+    print("success create phenotypes")
     # create data frame
     pheno_data = pd.DataFrame({
         'FID': sample_ids,
