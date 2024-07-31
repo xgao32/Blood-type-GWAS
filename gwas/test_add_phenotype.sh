7be023dec5c5440917401c8c4d9dcf38a1eba2ba
Xiaofeng Li <lixiaofeng0603@gmail.com>
2024-07-31 01:21:20 -0400

    test script for phenotype generate

--- gwas/test_add_phenotype.sh
+++ gwas/test_add_phenotype.sh
@@ -5,6 +5,6 @@ root_dir="/home/toronto/Blood-type-GWAS/gwas/"
 fam_file="${root_dir}test_chr9.fam"
 phenotype_file="${root_dir}test_chr9.txt"
 output_fam_file="${root_dir}updated_test_chr9.fam"
-script_file="${root_dir}vcf_to_plink.py"
+script_file="${root_dir}add_phenotype.py"
 
 python $script_file $fam_file $phenotype_file $output_fam_file
