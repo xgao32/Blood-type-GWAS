2887e2ae1ff765e49635898cce5379b4b9b03fdf
Xiaofeng Li <lixiaofeng0603@gmail.com>
2024-07-31 01:35:55 -0400

    test script for phenotype generate

--- gwas/add_phenotype.py
+++ gwas/add_phenotype.py
@@ -29,7 +29,7 @@ def update_plink_phenotype(fam_file, phenotype_file, output_fam_file):
     # Handle missing values: retain original phenotype values if new ones are missing
     updated_fam_data['Phenotype'].fillna(fam_data['Phenotype'], inplace=True)
     updated_fam_data['Phenotype'] = updated_fam_data['Phenotype'].apply(lambda x: fam_data['Phenotype'] if x == 'NA' else x)
-
+    print(updated_fam_data)
     # Drop the temporary 'NewPhenotype' column
     updated_fam_data = updated_fam_data.drop(columns=['NewPhenotype'])
 
