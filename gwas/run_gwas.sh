b4dc6096ae9590db6e1fd19990236beea1df0981
Xiaofeng <xiaof.li@mail.utoronto.ca>
2024-08-04 09:34:07 +0800

    update run_gwas

--- gwas/run_gwas.sh
+++ gwas/run_gwas.sh
@@ -15,9 +15,9 @@ convert_vcf_to_plink('$local_vcf_path', '$output_prefix')
 vcf_file="ALL.chr9.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
 output_file="${root_dir}/test_chr9.txt"
 chrom="9"
-pos=132136809
-ref="G"
-alt="A"
+pos=132136908
+ref="T"
+alt="TC"
 
 # Run the Python script to generate the phenotype file
 python generate_phenotype.py $vcf_file $chrom $pos $ref $alt $output_file
@@ -40,5 +40,6 @@ plink2 \
     --pheno ${phenotypeFile} \
     --pheno-name ${colName} \
     --maf 0.01 \
+        --glm allow-no-covars \
     --threads ${threadnum} \
     --out 1kgeas
